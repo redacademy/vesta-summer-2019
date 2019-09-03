3335d3964b7b40259c25907b2785ac1cda14d907
cdrosales <cara10denise@hotmail.com>
2019-09-03 15:11:48 -0400

    chore: add content to about

--- page-about.php
+++ page-about.php
@@ -1,21 +1,49 @@
 <?php get_header(); ?>
 
-<?php
-if( have_posts() ) :   
-    // Start WP Loop
-    while( have_posts() ) :   
-    the_post(); ?>         
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
+<section class="aboutUs">
 
-    <!-- end of wp loop -->
-    <?php endwhile; ?>
+    <h1> About vesta</h1>
+    <p>Vesta Social Innovation Technologies was created in response to 
+    the??? global movement. We aim to do what some say is impossible: </p>
+    <h3>Eliminate Rape Culture</h3>
 
-<?php the_posts_navigation(); ?> 
+    <div class="vmContainer">
+        <div class="vision-mission">
+            <h2>Our Vision & Mission</h2>
+            <h3> Provide a safe space for those who have experienced gender based 
+            violence or harassment.</h3>
+            <p>To give them a new level of control, connectivity and support. 
+            Our mission is to create a secure and trusted platform which will 
+            support folks' individual pathways to justice and to healing.</p>
+        </div>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+        <div class="ourApproach">
+            <h2>Our Approach</h2>
+                <h3>Vesta combines the expertise of clinical and legal experts with the 
+                knowledge of those with lived experiences and front line advocates.</h3>
+                
+                <p> Create??? a trauma-informed, survivor-centric process that provides 
+                individuals with a tool to document their experience, and create a 
+                reliable, time-stamped record of events.</p>
+        </div>
+    </div>
+
+    <div class="whyVesta">
+        <h2>Why Vesta</h2>
+            <ul class="whyVestaList">
+                <h3>
+                    <li>Data Protection </li>
+                    <li>Access to resources???</li>
+                    <li>Safe Space</li>
+                </h3>
+            </ul>
+    </div>
+
+
+    <h3>To learn how vest works, book a free information session</h3>
+    <p>Experience VESTA and how it works towards a safer work environment.</p>
+
+</section>
 
 <?php get_footer(); ?>
 
