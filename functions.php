60fbd1652f5dec784635f412b4aa7a34d870e08e
Kayla <kaynuhe@gmail.com>
2019-09-03 11:01:36 -0400

    chore: add custom fonts

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@
 // Add scripts and stylesheets
 function vesta_files() {
     wp_enqueue_style('vesta_workplace_styles', get_stylesheet_uri('/build/css/style.min.css'), NULL, microtime());
-    // wp_enqueue_style('fonts', "https://fonts.googleapis.com/css?family=Lato&display=swap");
     wp_enqueue_script('test_js', get_template_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
 };
 
