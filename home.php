2bd47cbb24000c95c9e086b33ed30da8201d893f
cdrosales <cara10denise@hotmail.com>
2019-09-03 17:23:33 -0400

    feat: add title content

--- home.php
+++ home.php
@@ -1,21 +1,29 @@
 <?php get_header(); ?>
 
-<?php
-if( have_posts() ) :   
-    // Start WP Loop
-    while( have_posts() ) :   
-    the_post(); ?>         
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
 
-    <!-- end of wp loop -->
-    <?php endwhile; ?>
+<section class="resources">
 
-<?php the_posts_navigation(); ?> 
+    <div class="resources-title-container">
+        <h1><?php the_title(); ?></h1> <!--not working, need to fix-->
+        <p> Educational resources and guides.</p>
+    </div>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+    <div class="resources-content-container">
+
+        <?php
+        if( have_posts() ) :   
+            while( have_posts() ) :   
+            the_post(); ?>         
+        <?php the_content(); ?>
+            <?php endwhile; ?>
+        <?php the_posts_navigation(); ?> 
+        <?php else : ?>
+                <p>No posts found</p>
+        <?php endif; ?>
+        
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
 
