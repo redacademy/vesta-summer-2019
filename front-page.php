6d421faa1a831238ea1029b7828af6286ef4b25c
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-04 14:37:56 -0400

    Feat: Added Front Page

--- front-page.php
+++ front-page.php
@@ -17,25 +17,96 @@ if( have_posts() ) :
         <p>No posts found</p>
 <?php endif; ?>
 
-<!-- Main Content of Front Page -->
-<section class="home">
-        <img src="<?php echo get_template_directory_uri() . '/Assets/Logo/Vesta_LOGO_LETTERS.png';?>">
+
+
+
+<main class="home">
+    <section class="home-transforming">
+        <img src="<?php echo get_template_directory_uri() . '/images/Homepage/img__homepage__hero.png';?>" alt="Vesta Woman">
         <h1>Transforming the workplace</h1>
         <p>Eliminating sexual harassment in the workplace by providing
         a secure reporting platform for employees and employers.</p>
         <div class="image-holder"></div>
-</section>
-
-
-
-
-    <div class="our-vision-mission">
-            <div class="headerStyleContainer">
-                <hr class="headerLine">
-                <h2>Our Vision & Mission</h2>
+    </section>
+    <section class="home-the-problem">
+        <div class="problem-article">
+            <div class="header-style-container">
+                <hr class="header-line">
+                <h2>The Problem</h2>
             </div>
+            <p>One credible complaint can decrease stock prices by as
+            much as 30%. Small and medium sized companies do not have 
+            the resources needed to mitigate the cost, risks and impact 
+            that a complaint or viral tweet can have to thier reputation.
+        </div>
+        <div class="problem-widget">
+        </div>
+    </section>
+    <section class="home-strength">
+        <div class="header-style-container">
+            <hr class="header-line">
+            <h2>Strength in Numbers</h2>
+        </div>
+        <div class="strength-number-95">
+            <hr class="strength-line">
+            <p>Of those who are sexually assaulted do not report.</p>
+        </div>
+        <div class="strength-number-80">
+            <hr class="strength-line">
+            <p>Of those who have unwanted experiences in the workplace 
+            do not report them.</p>
+        </div>
+        <div class="strength-number-85">
+            <hr class="strength-line">
+            <p>Of survivors disclose thier experience informally but 
+            not officially for fear of retaliation, judgement,
+            coercion, or exclusion.</p>
+        </div>
+    </section>
+    <section class="home-solution">
+        <img class="solution-image" src="<?php echo get_template_directory_uri() . '/images/Homepage/img__homepage__thesolution.png';?>" alt="Vesta on devices">
+        <div class="inner-solution">
+            <div class="header-style-container">
+                <hr class="header-line">
+                <h2>The Solution</h2>
+            </div>
+                <p> Vesta interacts with the individual to help them share 
+                thier experience and actively participate in thier own case,
+                should they choose to. We provide an easy to use and safe 
+                online platform.</p>
+                <input type="submit" class="what-we-offer-button" value="What We Offer">
+        </div>
+    </section>
+    <section class="home-customer-stories">
+        <div class="header-style-container">
+                    <hr class="header-line">
+                    <h2>Our Customer Stories</h2>
+        </div>
+        <div class="inner-customer-stories">
+            <div class="customer-stories-title">
+            </div>
+            <div class="customer-stories-content">
+            </div>
+        </div>
+    </section>
+    <section class="trusted-partners">
+        <div class="partners-list">
+            <img src="<?php echo get_template_directory_uri() . '/images/Partners/logo-canada.png';?>" alt="GOvernment of Canada">
+            <img src="<?php echo get_template_directory_uri() . '/images/Partners/legal-innovation-zone-logo.png';?>" alt="Legal Innovation Zone">
+            <img src="<?php echo get_template_directory_uri() . '/images/Partners/social-venture-zone-logo.jpg';?>" alt="Social Venture Zone">
+            <img src="<?php echo get_template_directory_uri() . '/images/Partners/unilever_edited.png';?>" alt="Unilever Impact">
+        </div>
+    </section>
+    <section class="home-subscribe">
+    <h2> Subscribe to get the latest updates</h2>
+    <form class="subscribe-form">
+            <label>Enter your Email address</p>
+            <input type="text" class="subscribe-email" placeholder="johndoe@gmail.com">
+            <input type="submit" class="subscribe-button" value="Subscribe">
+        </form>
+    </section>
 
-
+</main>
 
 <?php get_footer(); ?>
 
