ea9d0db9b1b2cf41498cb0603bf692fd4d7e8274
Kayla <kaynuhe@gmail.com>
2019-09-04 10:30:40 -0400

    chore: clean js console.logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 (function($) {
-    console.log('Houston, we have a connection.')
     const $faqIcon = $('.toggle-answer')
 
-    // Toggle FAQ
+    // Toggle FAQ - allow multiple open
     $faqIcon.on('click', (e) => {
-        console.log('this:', this)
-        console.log($(e.target).siblings('div.faq-answer'))
         $(e.target).siblings('div.faq-answer').slideToggle(); 
     })
 
