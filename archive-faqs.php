b7aa85de2ad4a18714152d09ac00b0160bdedf7f
kbaistrocchi <51097981+kbaistrocchi@users.noreply.github.com>
2019-09-04 16:33:47 -0400

    Revert "Feat/activate sidebars"

--- archive-faqs.php
+++ archive-faqs.php
@@ -1,40 +1,34 @@
 <?php get_header(); ?>
 
-<?php
-$terms = get_terms('faq_category');
-foreach($terms as $term) : ?>
+<?php 
+$faqs = new WP_Query( array('post-type' => 'faqs'));
+while($faqs->have_posts()) :
+    $faqs->the_post(); ?>
+    <?php $cats =  get_the_category(); 
+    echo '<pre>';
+    var_dump($cats); 
+    echo '</pre>'; ?>
+    <?php foreach((get_the_category()) as $category) : ?>
     <div class="faq-category">
-        <h3><?php echo $term->name;?></h3>
-        <hr>
+        <h2><?php echo $category->cat_name ?></h2>
     </div>
-    <div class="faq-list"></div>
+<?php endforeach; ?>
+
+<?php endwhile; ?>
+
+
+
+<!-- if( $my_query->have_posts() ) {
+    while ($my_query->have_posts()) : $my_query->the_post(); ?>
+    <?php foreach((get_the_category()) as $category) {echo $category->cat_name . ' ';} ?><br>
+    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a><br>
     <?php
-    $faqs = new WP_Query(array(
-        'post_type' => 'faqs',
-        'order' => 'DESC',
-        'tax_query' => array(
-            array(
-                'taxonomy' => 'faq_category',
-                'field' => 'name',
-                'terms' => $term->name
-            ),
-        ),
-    ));
-    while($faqs->have_posts() ) :   
-        $faqs->the_post(); ?>
-        <div class="faq-single">
-            <p><?php the_title(); ?></p><i class="fas fa-plus toggle-answer"></i><i class="fas fa-minus toggle-answer"></i>
-            <hr>
-            <div class="faq-answer"><?php the_content(); ?></div>
-        </div>
-        
+    // endwhile;
+// } -->
 
-    <?php endwhile; ?>
-    <?php wp_reset_query(); ?>
-    <?php endforeach; ?>
+wp_reset_query();
 
-<!-- <?php the_posts_navigation(); ?>  -->
-
+?>
 
 <?php get_footer(); ?>
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
 (function($) {
-    const $faqIcon = $('.toggle-answer')
-    const $plusIcon = $('.fa-plus')
-    const $minusIcon = $('.fa-minus')
-
-    // Toggle FAQ - allow multiple open
-    $faqIcon.on('click', (e) => {
-        $(e.target).toggle()
-        $(e.target).siblings('.toggle-answer').toggle()
-        $(e.target).siblings('div.faq-answer').slideToggle(); 
-    })
+    console.log('Houston, we have a connection.')
 
 })(jQuery);
