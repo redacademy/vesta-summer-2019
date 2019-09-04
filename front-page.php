24ee72f0ce6561f294e8022a91785c28df1ddeba
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-04 15:03:35 -0400

    Feat:Added What We Offer Page

--- front-page.php
+++ front-page.php
@@ -20,22 +20,6 @@ if( have_posts() ) :
 
 <!-- Main Content of Front Page -->
 <section class="home">
-<img src="./Assets/" alt="">
-        <h1>Transforming the workplace</h1>
-        <p>Eliminating sexual harassment in the workplace by providing
-        a secure reporting platform for employees and employers.</p>
-        <div class="image-holder"></div>
-</section>
-
-
-
-
-    <div class="our-vision-mission">
-            <div class="headerStyleContainer">
-                <hr class="headerLine">
-                <h2>Our Vision & Mission</h2>
-            </div>
-
 
 
 <?php get_footer(); ?>
--- page-what-we-offer.php
+++ page-what-we-offer.php
@@ -17,6 +17,23 @@ if( have_posts() ) :
         <p>No posts found</p>
 <?php endif; ?>
 
+<main class="what-we-offer">
+    <h1>What We Offer</h1>
+    <p>Our mission is to eliminate sexual harassment in the workplace
+    by providing educational resources and easy-to-use and secure
+    reporting platform for employees and employers.</p> 
+    <div class="what-we-offer-private">
+        <img src="<?php echo get_template_directory_uri() . '/Assets/Icons/IconLinkedIn.svg';?>">
+    </div>
+
+</main>
+
+
+
+
+
+
+
 <?php get_footer(); ?>
 
                         
