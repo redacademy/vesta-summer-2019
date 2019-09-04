173d5c5e40ae9223edd47a563ea3ecc7852cd564
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-04 10:22:50 -0400

    Feat: changed icon url

--- footer.php
+++ footer.php
@@ -10,19 +10,21 @@ wp_footer();
         </div>
 
         <div class="footer-subscribe">
-            <p> subscribe to get the latest updates about Vesta</p>
-            <textarea class="subscribe-email">johndoe@gmail.com</textarea>
-            <button class="subscribe-button"> Subscribe</button>
+        <form class="subscribe-form">
+            <label> subscribe to get the latest updates about Vesta</p>
+            <input type="text" class="subscribe-email" placeholder="johndoe@gmail.com">
+            <input type="submit" class="subscribe-button" value=" Subscribe">
+        </form>
         </div>
     </div>
 
     <div class="lower-footer">
         <div class="footer-nav-2"></div>
         <div class="socials-footer">
-            <img classs="twitter" src="<?php echo get_template_directory_uri() . '/Assets/Icons/IconTwitter.svg';?>">
-            <img classs="facebook" src="<?php echo get_template_directory_uri() . '/Assets/Icons/IconFacebook.svg';?>">
-            <img classs="instagram" src="<?php echo get_template_directory_uri() . '/Assets/Icons/IconInstagram.svg';?>">
-            <img classs="linkedin" src="<?php echo get_template_directory_uri() . '/Assets/Icons/IconLinkedIn.svg';?>">
+            <img classs="twitter" src="<?php echo get_template_directory_uri() . '/images/Icons/IconTwitter.svg';?>">
+            <img classs="facebook" src="<?php echo get_template_directory_uri() . '/images/Icons/IconFacebook.svg';?>">
+            <img classs="instagram" src="<?php echo get_template_directory_uri() . '/images/Icons/IconInstagram.svg';?>">
+            <img classs="linkedin" src="<?php echo get_template_directory_uri() . '/images/Icons/IconLinkedIn.svg';?>">
         </div>
     </div>
 
