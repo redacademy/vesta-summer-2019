e8ca3b945754e8379e218f2a0bd91f5d00249636
Kayla <kaynuhe@gmail.com>
2019-09-04 13:02:18 -0400

    feat: toggle icons on FAQ click

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 (function($) {
     const $faqIcon = $('.toggle-answer')
+    const $plusIcon = $('.fa-plus')
+    const $minusIcon = $('.fa-minus')
 
     // Toggle FAQ - allow multiple open
     $faqIcon.on('click', (e) => {
+        $(e.target).toggle()
+        $(e.target).siblings('.toggle-answer').toggle()
         $(e.target).siblings('div.faq-answer').slideToggle(); 
     })
 
