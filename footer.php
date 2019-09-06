654909aaa43c3c88879f2d4186d55f1093285bd3
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-06 14:48:08 -0400

    feat:Footer Styling

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@ wp_footer();
 <div class="footer">
     <div class="footer-inner-container">
         <div class="upper-footer">
-            <div class="footer-logo"></div>
+            <div class="footer-logo"><img src="<?php echo get_template_directory_uri(). '/images/Icons/IconInstagram.svg';?>"></div>
             <div class="footer-nav"><?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>  </div>
         </div>
 
@@ -21,10 +21,10 @@ wp_footer();
     <div class="lower-footer">
         <div class="footer-nav-2"></div>
         <div class="socials-footer">
-            <a href="https://twitter.com/vestasit" target="_blank"><img classs="twitter" src="<?php echo get_template_directory_uri() . '/images/Icons/IconTwitter.svg';?>"></a>
-            <a href="https://www.facebook.com/VestaSIT/" target="_blank"><img classs="facebook" src="<?php echo get_template_directory_uri() . '/images/Icons/IconFacebook.svg';?>"></a>
-            <a href="https://www.instagram.com/vesta.s.i.t/" target="_blank"><img classs="instagram" src="<?php echo get_template_directory_uri() . '/images/Icons/IconInstagram.svg';?>"></a>
-            <a href="https://www.linkedin.com/company/vesta-social-innovation-technologies/" target="_blank"><img classs="linkedin" src="<?php echo get_template_directory_uri() . '/images/Icons/IconLinkedIn.svg';?>"></a>
+            <a href="https://twitter.com/vestasit" target="_blank"><img classs="twitter" src="<?php echo get_template_directory_uri() . '/images/Icons/IconTwitter.svg';?>" alt="Twitter Icon"></a>
+            <a href="https://www.facebook.com/VestaSIT/" target="_blank"><img classs="facebook" src="<?php echo get_template_directory_uri() . '/images/Icons/IconFacebook.svg';?>" alt="Facebook Icon"></a>
+            <a href="https://www.instagram.com/vesta.s.i.t/" target="_blank"><img classs="instagram" src="<?php echo get_template_directory_uri() . '/images/Icons/IconInstagram.svg';?>" alt="Instagram Icon"></a>
+            <a href="https://www.linkedin.com/company/vesta-social-innovation-technologies/" target="_blank"><img classs="linkedin" src="<?php echo get_template_directory_uri() . '/images/Icons/IconLinkedIn.svg';?>" alt="LinkedIn Icon"></a>
         </div>
     </div>
 
