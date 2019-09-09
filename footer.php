92598c48406822d5966535dce2f2ecb0985a5176
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-09 11:50:05 -0400

    Style: Styling on footer sub-menu

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@ wp_footer();
 
         <div class="footer-subscribe">
         <form class="subscribe-form">
-            <label> Subscribe to get the latest updates about Vesta</p>
+            <label> Subscribe to get the latest updates about Vesta</label>
             <input type="text" class="subscribe-email" placeholder="johndoe@gmail.com">
             <input type="submit" class="subscribe-button" value=" Subscribe">
         </form>
@@ -20,7 +20,7 @@ wp_footer();
     </div>
 
     <div class="lower-footer">
-        <div class="footer-nav-2"></div>
+        <div class="footer-nav-2"><?php wp_nav_menu( array( 'theme_location' => 'sub-footer' ) ); ?></div>
         <div class="socials-footer">
             <a href="https://twitter.com/vestasit" target="_blank"><i class="fab fa-twitter"></i></a>
             <a href="https://www.facebook.com/VestaSIT/" target="_blank"><i class="fab fa-facebook-square"></i></a>
