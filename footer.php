5e44aabbc9c53ca3373b9745e9dbfd43f74ef7bd
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-09 01:29:22 -0400

    Style: Added styling on Footer

--- footer.php
+++ footer.php
@@ -2,16 +2,17 @@
 wp_footer();
 ?>
 <!-- scripts will load into this wp_footer() -->
-<div class="footer">
+<section class="footer">
     <div class="footer-inner-container">
         <div class="upper-footer">
-            <div class="footer-logo"><img src="<?php echo get_template_directory_uri(). '/images/Homepage/';?>"></div>
+            <div class="footer-logo"><img class="logo-black" src="<?php echo get_template_directory_uri(). '/images/Icons/Vesta_Horizontal_Black.png';?>">
+                                     <img class="logo-letter" src="<?php echo get_template_directory_uri(). '/images/Icons/Vesta_LOGO_LETTERS.png';?>"></div>
             <div class="footer-nav"><?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>  </div>
         </div>
 
         <div class="footer-subscribe">
         <form class="subscribe-form">
-            <label> subscribe to get the latest updates about Vesta</p>
+            <label> Subscribe to get the latest updates about Vesta</p>
             <input type="text" class="subscribe-email" placeholder="johndoe@gmail.com">
             <input type="submit" class="subscribe-button" value=" Subscribe">
         </form>
@@ -28,7 +29,7 @@ wp_footer();
         </div>
     </div>
 
-</div>
+</section>
 
 
     
