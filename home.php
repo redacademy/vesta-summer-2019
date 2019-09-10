b246a84598ea691f91516e08b40aed320c99d673
Kayla <kaynuhe@gmail.com>
2019-09-10 16:48:59 -0400

    feat: display sub cat postsonly

--- home.php
+++ home.php
@@ -12,105 +12,51 @@
 
         <section class="community-resources">
 
-            <div class="community-videos">
-                <h3> Videos </h3>
-                    <?php
-                    // Community Video
-                    $cats = get_posts(array(
-                        'category_name' => 'video-community',
-                        'posts_per_page' => 3,
+            <?php 
+            $space = get_category_by_slug( 'community' );
+            echo 'Community Cat';
+            echo '<pre>';
+            print_r($space);
+            echo '</pre>';
+
+
+            echo 'sub categories';
+            $sub_cats = get_categories(array(
+                'child_of' => $space->term_id,
+            ));
+            echo '<pre>';
+            print_r($sub_cats);
+            echo '</pre>';
+
+
+            foreach($sub_cats as $sub_cat) : ?>
+            <?php var_dump($sub_cat); ?>
+
+                <?php
+                $posts = get_posts(array(
+                        'category_name' => $sub_cat->name,
+                        'post_type' => 'post',
                         'post_status' => 'published', 
                         'orderby' => 'date', 
-                        
                     ));
+                    echo 'posts';
+                    echo '<pre>';
+                    print_r($posts);
+                    echo '</pre>';
 
-                    foreach ($cats as $cat){
-                        $content = $cat->post_content;
-                        $media = get_media_embedded_in_content( $content );
-                        echo '<div class="media-community">';
-                        echo $media[0];
-                        echo '</div>';
-
-                        echo '<p>';
-                        echo $cat->post_excerpt;  
-                        echo '</p>'; 
-                    }
                     ?>
-            </div>
+                    
+                    
+                <!-- </div> -->
+            <?php endforeach; ?>
 
-
-            <div class="community-articles">
-                <h3> Articles </h3>
-                    <?php
-                    //Community Articles
-                    $cats = get_posts(array(
-                        'category_name' => 'article-community',
-                        'posts_per_page' => 3,
-                        'post_status' => 'published', 
-                        'orderby' => 'date', 
-                    ));
-
-                    foreach ($cats as $cat){
-                        echo '<p>';
-                        echo $cat->post_excerpt;  
-                        echo '</p>';  
-                        echo get_the_post_thumbnail($cat);
-                    }?>
-            </div>
+            
+          
         </section>
 
         <section class="workplace-resources">
 
-            <div class="workplace-videos">
-                <h3> Videos </h3>
-                    <?php 
-                    // Workplace Video
-                    $cats = get_posts(array(
-                        'category_name' => 'video-workplace',
-                        'posts_per_page' => 3,
-                        'post_status' => 'published', 
-                        'orderby' => 'date', 
-                    ));
-
-                    foreach ($cats as $cat){
-                        $content = $cat->post_content;
-                        $media = get_media_embedded_in_content( $content );
-                        echo '<div class="media-workplace">';
-                        echo $media[0];
-                        echo '</div>';
-
-
-                        echo '<p>';
-                        echo $cat->post_excerpt;  
-                        echo '</p>';  
-                    }?>
-            </div>
-
-
-            <div class="workplace-articles">
-                <h3> Articles </h3>
-
-
-                    <?php
-                    //Workplace Article
-                    $cats = get_posts(array(
-                        'category_name' => 'article-workplace',
-                        'posts_per_page' => 3,
-                        'post_status' => 'published', 
-                        'orderby' => 'date', 
-                    ));
-
-                    foreach ($cats as $cat){
-                        echo '<p>';
-                        echo $cat->post_excerpt;  
-                        echo '</p>';  
-                        echo get_the_post_thumbnail($cat);
-                    }
             
-                    ?>
-
-
-            </div>
 
         </section>
 
@@ -138,4 +84,4 @@
 
 <?php get_footer(); ?>
 
-                        
+    
