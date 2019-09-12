567ec8e862cd894347a50bffd2aa75a434469922
cdrosales <cara10denise@hotmail.com>
2019-09-12 16:22:45 -0400

    fix: fix file path for vesta logo

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 
 <header>
     <a href="<?php echo get_home_url(); ?>">
-    <img src="<?php echo get_template_directory_uri() . '/images/icons/vesta_logo.png'; ?>" class="header-logo"/>
+    <img src="<?php echo get_template_directory_uri() . '/images/icons/vest_logo.png'; ?>" class="header-logo"/>
     </a>
     <img src="<?php echo get_template_directory_uri() . '/images/icons/Vesta_LOGO_LETTERS.png'; ?>" class="header-logo-letter"/>
 
