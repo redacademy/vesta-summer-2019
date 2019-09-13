f8d832532cc1cfec6133d957c502d12f8eaca797
cdrosales <cara10denise@hotmail.com>
2019-09-13 11:15:18 -0400

    feat: add 404 style page

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<section class="404-page">
-    <img class="404-icon" src="<?php echo get_template_directory_uri() . '/images/Icons/404.png';?>">
-    <h2 class="404">404</h2>
-    <h1 class="404-page-not-found">Page Not Found</h1>
+<section class="error-page">
+    <img class="error-icon" src="<?php echo get_template_directory_uri() . '/images/Icons/404.png';?>">
+    <h2 class="error">404</h2>
+    <h1 class="error-page-not-found">Page Not Found</h1>
     <p>The page are looking for doesn't exist.
     Click <a href="<?php echo esc_url(home_url( '/' ));?>">here</a> to go to Home Page</p>
 </section>
