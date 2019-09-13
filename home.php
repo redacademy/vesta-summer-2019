e94d15e1d086fb3cca57c23fadb1eeb3e677f089
Kayla <kaynuhe@gmail.com>
2019-09-13 11:28:28 -0400

    feat: resources get videos

--- home.php
+++ home.php
@@ -20,86 +20,71 @@
             ));
 
             foreach($sub_cats as $sub_cat) : ?>
-            <h3><?php echo $sub_cat->name; ?></h3>
+                <h3><?php echo $sub_cat->name; ?></h3>
 
                 <?php
-                $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
                 $args = array(
                     'post_type' => 'post',
                     'post_status' => 'publish', 
                     'orderby' => 'date',
-                    'posts_per_page' => '3&paged=' . $paged,
+                    'posts_per_page' => 3,
                     'tax_query' => array(
                         array(
                             'taxonomy' => 'category',
                             'field' => 'name',
                             'terms' => $sub_cat->name
-                        )
-                        )
+                        ))
                     );
 
                     
 
-                $posts = new WP_Query($args);
-                if ( $posts->have_posts() ) :?>
-                <?php while($posts->have_posts()) :
-                //     $posts->the_post(); 
-                //     $post_ID = get_the_ID();
-                    // $post_meta = get_post_meta($post_ID);
-                    // echo '<pre>';
-                    // var_dump($post_meta);
-                    // echo '</pre>';
-                    ?>
-                    <div class="community-resource">
+                    $posts = new WP_Query($args);
+                    if ( $posts->have_posts() ) :?>
+                    <?php while($posts->have_posts()) :
+                        $posts->the_post(); 
+                    //     $post_ID = get_the_ID();
+                        // $post_meta = get_post_meta($post_ID);
+                        // echo '<pre>';
+                        // var_dump($post_meta);
+                        // echo '</pre>';
+                        ?>
+                        <div class="single-community-resource">
+                            
                         
+                            <?php if($sub_cat->name === "Videos") : ?>
+                                <div class="video resource-media">
+                                    <?php 
+                                    $content = get_the_content();
+                                    $media = get_media_embedded_in_content( $content ); 
+                                    echo $media[0];
+                                    ?>
+                                </div>
+
+                            <?php elseif($sub_cat->name === "Audio") : ?>
+                                <div class="audio resource-media">
+                                    <?php echo 'this is where an audio image will appear'; ?>
+                                    <!-- get audio clip -->
+                                </div>
+
+                            <?php else : ?>
+                                <div class="resource-media">
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
+                            <?php endif; ?>
+                            <div><?php the_excerpt(); ?></div>
+                        </div>
                     
-                        <?php if($sub_cat->name === "Videos") : ?>
-                            <div class="video resource-media">
-                                <?php echo 'this is where a video will appear'; ?>
-                                <!-- get video -->
-                            </div>
 
-                        <?php elseif($sub_cat->name === "Audio") : ?>
-                            <div class="audio resource-media">
-                                <?php echo 'this is where an audio image will appear'; ?>
-                                <!-- get audio clip -->
-                            </div>
+                <?php endwhile; ?> 
+                    
+                <?php wp_reset_postdata(); ?>
 
-                        <?php else : ?>
-                            <div class="resource-media">
-                                <?php the_post_thumbnail(); ?>
-                            </div>
-                        <?php endif; ?>
-                        <div><?php the_excerpt(); ?></div>
-                    </div>
-                   
-
-               <?php endwhile; ?> 
-               <div class="pagination">
-               <!-- // Previous/next page navigation. -->
-                    <?php the_posts_pagination( array(
-                        'prev_text'  => __('&laquo;'),
-                        'next_text'  => __('&raquo;'),
-                    ) ); ?>
-                
-
-
-               <?php wp_reset_postdata(); ?>
-                    <?php endif; ?>
+                <?php endif; ?>
             
-                    
-                <!-- </div> -->
-            <?php endforeach; ?>
+               
+        <?php endforeach; ?>
 
-            <?php 
-                    
-                ?>
-                </div> 
 
-            
-             
-
-          
             </section>
 
             <section class="workplace-resources">
@@ -111,7 +96,7 @@
 
 
 
-    <h3> Can't find what you're looking for?</h3> 
+         <h3> Can't find what you're looking for?</h3> 
 
         <form action="" method="post" class="resources-form">
             <h3>Name:</h3>
