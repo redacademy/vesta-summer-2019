db08cc5c5275f1ad4b1fc6c030b6ed680125954d
cdrosales <cara10denise@hotmail.com>
2019-09-13 11:41:08 -0400

    style: add styles to 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
 
 <section class="error-page">
-    <img class="error-icon" src="<?php echo get_template_directory_uri() . '/images/Icons/404.png';?>">
-    <h2 class="error">404</h2>
-    <h1 class="error-page-not-found">Page Not Found</h1>
-    <p>The page are looking for doesn't exist.
-    Click <a href="<?php echo esc_url(home_url( '/' ));?>">here</a> to go to Home Page</p>
+    <div class="error-container">
+        <img class="error-icon" src="<?php echo get_template_directory_uri() . '/images/Icons/404.png';?>">
+        <h2>404</h2>
+        <h1>Page Not Found</h1>
+        <p>The page are looking for doesn't exist.
+        <br>
+        Click <a href="<?php echo esc_url(home_url( '/' ));?>">here</a> to go to Home Page</p>
+    </div>
 </section>
 
 <?php get_footer(); ?>
