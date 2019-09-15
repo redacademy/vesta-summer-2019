5f385702ffff778d46b7d17cae6f89cf502b779b
cdrosales <cara10denise@hotmail.com>
2019-09-14 20:07:46 -0400

    style: use grid to style category

--- category.php
+++ category.php
@@ -1,22 +1,35 @@
 <?php get_header(); ?>
 
-<?php
-if( have_posts() ) :   
-    // Start WP Loop
-    while( have_posts() ) :   
-    the_post(); ?>         
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
+<div class="category-container">
+    <div class="category-title-wrapper">
+        <h2><?php echo single_cat_title() ?></h2>
+        
+    </div>
 
-    <!-- end of wp loop -->
-    <?php endwhile; ?>
+    <div class="category-content-container">
+        <?php
+        if( have_posts() ) :   
+            while( have_posts() ) :   
+            the_post(); ?>    
+        <div class="category-content-wrapper">
+            <div class="category-content">
+                <a href="<?php the_permalink(); ?>">
 
-<?php the_posts_navigation(); ?> 
+                    <div class="post-thumbnail"?><?php the_post_thumbnail('full'); ?></div>
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_excerpt(); ?>
+                </a>
+            </div>
+        </div>
 
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif; ?>
+
+            <?php endwhile; ?>
+        <?php the_posts_navigation(); ?> 
+
+        <?php else : ?>
+                <p>No posts found</p>
+        <?php endif; ?>
+            </div>
+</div>
 
 <?php get_footer(); ?>
-
-                        
